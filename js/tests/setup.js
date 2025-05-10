d79e011ce7717778f3e7bef956b28bac2f6e379b
ZynCarpets <zyncarpetcare@gmail.com>
2025-05-09 23:36:31 -0500

    Upload files

--- js/tests/setup.js
+++ js/tests/setup.js
@@ -1,4 +1,8 @@
 // Test setup file
+const { TextEncoder, TextDecoder } = require('util');
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+
 const { JSDOM } = require('jsdom');
 
 // Create a new JSDOM instance
